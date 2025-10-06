d8142d1745db49b94ce3b62d0bf65dc88ffd8123
Shemeera Fonseka <shemeerafonseka@gmail.com>
2025-10-06 20:33:05 +0530

    deploy edits

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import Topbar from './components/Topbar/Topbar';
 import Home from './components/Home/Home';
 import BodyCanvasAnimation from './components/BodyCanvasAnimation/BodyCanvasAnimation';
 import About from './components/About/About';
